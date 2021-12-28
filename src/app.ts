ffd32040d729881dcc8e230cc9038c3f286dcc91
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-28 12:55:22 -0300

    Autenticando via github

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
+import "dotenv/config"
+import { Request, Response } from "express";
+
+const github_id = process.env.GITHUB_CLIENT_ID
+
+
 const PORT = 4000;
 import express from "express"
 
 const app = express()
+
+
+app.get("/github", (request: Request, response: Response) => {
+    console.log(github_id)
+    console.log("Chegou aqui")
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${github_id}`)
+})
+
+app.get("/signin/callback", (req: Request, res: Response) => {
+    console.log("chegou aqui 2")
+    const {code} = req.query;
+    return res.json(code)
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
