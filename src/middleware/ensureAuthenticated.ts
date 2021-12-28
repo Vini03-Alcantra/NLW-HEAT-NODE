5506149557588ca2e289092bd78ed523b6bf74d2
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-28 20:28:44 -0300

    Messages Controller funcionando

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken"
+
+interface IPayload {
+    sub: string;
+}
+
+export function ensureAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    const authToken = req.headers.authorization
+    const JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : ""
+
+
+    if(!authToken){
+        return res.status(401).json({
+            errorCode: "token.invalid"
+        })
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try{
+        const {sub} = verify(token, JWT_SECRET) as IPayload;
+
+        req.user_id = sub
+
+        return next()
+    }catch(err){
+        return res.status(401).json({errorCode: "token.expired"})
+    }
+}
+
