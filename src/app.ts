7d186fab929158d5d4146779559f8267c7d9f64f
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-28 21:21:46 -0300

    Configurando websocket

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
 import "dotenv/config"
 import express, { Request, Response } from "express";
+import http from "http"
+import { Server } from "socket.io";
+import cors from "cors"
 
 import { router } from "./routes";
 
 const github_id = process.env.GITHUB_CLIENT_ID
-const PORT = 4000;
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`)
+})
 
 app.get("/github", (request: Request, response: Response) => {    
     response.redirect(`https://github.com/login/oauth/authorize?client_id=${github_id}`)
@@ -19,4 +34,4 @@ app.get("/signin/callback", (req: Request, res: Response) => {
     return res.json(code)
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+export {serverHttp, io}
